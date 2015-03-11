7f89571f834e0b37b62d11f2725a065dd91107af
Andreas Madsen <amwebdk@gmail.com>
2015-03-11 16:01:33 +0100

    [explore] add sequence dataset generator

--- explore_theano/datasets.py
+++ explore_theano/datasets.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+
 def generate_quadrant(items):
     X = np.random.uniform(low=-1, high=1, size=(items, 2)).astype('float32')
 
@@ -9,3 +10,12 @@ def generate_quadrant(items):
     t += np.all([X[:, 0] < 0, X[:, 1] < 0], axis=0) * 2
     t += np.all([X[:, 0] >= 0, X[:, 1] < 0], axis=0) * 3
     return (X, t)
+
+
+def generate_accumulated(items, T=5):
+    X = np.random.randint(0, 11, size=(items, 2, T))
+
+    t = np.sum(X, axis=1)
+    t = np.cumsum(t, axis=1)
+    t = np.mod(t, 10)
+    return (X.astype('int32'), t.astype('int32'))
