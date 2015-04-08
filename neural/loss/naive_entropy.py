b58e7e272fa4709ebcbe5fc2f8c9f1fa2f801e30
Andreas Madsen <amwebdk@gmail.com>
2015-04-08 17:01:09 +0200

    [feature] support entopy on non-sequence output

--- neural/loss/naive_entropy.py
+++ neural/loss/naive_entropy.py
@@ -4,13 +4,17 @@ import theano
 import theano.tensor as T
 
 class NaiveEntropy:
-    def __init__(self):
+    def __init__(self, time=True):
+        self._time = time
         pass
 
     def setup(self, batch_size):
         self.batch_size = batch_size
 
     def loss(self, y, t):
-        t = t.ravel()
-        y = y.transpose(0, 2, 1).reshape((y.shape[2] * y.shape[0], y.shape[1]))
+        # If there is a time dimension, reshape the input such that the
+        # features and time appears in the same row.
+        if (self._time):
+            t = t.ravel()
+            y = y.transpose(0, 2, 1).reshape((y.shape[2] * y.shape[0], y.shape[1]))
         return T.mean(T.nnet.categorical_crossentropy(y, t))
