c3036c6b31e8e5d3eed214cc2fb324ca48398e0e
Andreas Madsen <amwebdk@gmail.com>
2015-04-28 13:41:48 +0200

    [feature] add dataset module

---
+++ dataset/encoder.py
@@ -0,0 +1,28 @@
+
+import os.path as path
+import os
+import numpy as np
+
+from sklearn.datasets import fetch_mldata
+
+thisdir = path.dirname(path.realpath(__file__))
+data_dir = path.join(thisdir, 'data')
+
+class Dataset():
+    def __init__(self, data, target, classes):
+        self.data = data
+        self.target = target
+        self.n_classes = classes
+
+def mnist():
+    data = fetch_mldata('MNIST original', data_home=data_dir)
+    shuffle = np.random.permutation(data.data.shape[0])
+
+    obs = data.data.shape[0]
+    time = data.data.shape[1]
+
+    return Dataset(
+        data.data.reshape(obs, 1, time).astype('float32')[shuffle, :],
+        data.target.astype('int32')[shuffle],
+        np.unique(data.target).size
+    )
