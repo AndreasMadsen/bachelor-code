d2a0c3901183ec32d628c6b33cffda3158524bda
Andreas Madsen <amwebdk@gmail.com>
2015-05-22 20:07:08 +0200

    [fix] expose distance and word2vec models

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,7 @@
 
 from model.connectivity import Connectivity
 from model.sparse import load, save
+from model.word2vec import Word2Vec
+from model.distance import Distance
 
-__all__ = ['Connectivity']
+__all__ = ['Connectivity', 'load', 'save', 'Word2Vec', 'Distance']
