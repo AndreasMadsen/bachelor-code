a1f78ebdb21996be79dfe4666d41caabbee5c882
Andreas Madsen <amwebdk@gmail.com>
2015-05-22 14:58:15 +0200

    [fix] limit to 1000000 observations

--- run/build_connectivity.py
+++ run/build_connectivity.py
@@ -6,5 +6,5 @@ import model
 import os.path as path
 
 m = model.Connectivity(verbose=True)
-connectivity = m.transform(dataset.news.dates().data)
+connectivity = m.transform(dataset.news.dates(100000).data)
 model.save(path.join(run.output_dir, 'builds/connectivity.hd5'), connectivity)
