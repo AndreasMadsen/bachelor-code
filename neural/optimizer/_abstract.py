974186f9cb95c12f510b9bd1c79b6cc0db8cac2b
Andreas Madsen <amwebdk@gmail.com>
2015-05-28 14:26:49 +0200

    [feature] add clipping as a clipping stratagy

--- neural/optimizer/_abstract.py
+++ neural/optimizer/_abstract.py
@@ -34,7 +34,7 @@ class OptimizerAbstract:
     def each_update(self, Wi, gWi):
         raise NotImplemented
 
-    def _clip_max(self, gW):
+    def _scale_max(self, gW):
         # Clips gW such that max(|gW|) \in [-clip, clip]
         max_value = 0
         for gWi in gW:
@@ -43,21 +43,33 @@ class OptimizerAbstract:
 
         return [gWi * scale for gWi in gW]
 
-    def _clip_L2(self, gW):
+    def _scale_L2(self, gW):
         # Clips gW such that ||gW||_2 \in [-clip, clip]
         norm = T.sqrt(T.sum([T.sum(gWi ** 2) for gWi in gW]))
         scale = T.minimum(1, self._clip / norm)
 
         return [gWi * scale for gWi in gW]
 
+    def _clip_within(self, gW):
+        return [
+            T.maximum(T.minimum(gWi, self._clip), -1 * self._clip)
+            for gWi in gW
+        ]
+
     def update(self, W, gW):
         """
         Generate update equations for the weights
         """
         if (self._clipping_stratagy == 'max'):
-            gW = self._clip_max(gW)
+            gW = self._scale_max(gW)
         elif (self._clipping_stratagy == 'L2'):
-            gW = self._clip_L2(gW)
+            gW = self._scale_L2(gW)
+        elif (self._clipping_stratagy == 'clip'):
+            gW = self._clip_within(gW)
+        elif (self._clipping_stratagy is not None):
+            raise NotImplementedError(
+                'clipping stratagy %s is not implemented' % self._clipping_stratagy
+            )
 
         return list(itertools.chain(*[
             self.each_update(Wi, gWi) for (Wi, gWi) in zip(W, gW)
