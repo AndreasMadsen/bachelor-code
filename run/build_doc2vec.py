0ea3406875c13001cbc2bf44f0606f9fdd210b82
Andreas Madsen <amwebdk@gmail.com>
2015-05-28 14:27:21 +0200

    [fix] doc2vec on all documents but subset the output

--- run/build_doc2vec.py
+++ run/build_doc2vec.py
@@ -7,5 +7,5 @@ import os.path as path
 
 m = model.Doc2Vec(verbose=True, workers=3)
 
-vecs = m.fit_transform(dataset.news.words(100000))
-model.save(path.join(run.output_dir, 'builds/doc2vec.vecs.npy'), vecs)
+vecs = m.fit_transform(dataset.news.words(full=True))
+model.save(path.join(run.output_dir, 'builds/doc2vec-full.vecs.npy'), vecs[0:100000, :])
