8c5d5007fc9b33ba4c649fc0828dec92755bd1a0
Andreas Madsen <amwebdk@gmail.com>
2015-06-02 12:15:41 +0200

    [fix] pca plot ready for report

--- plot/pca.py
+++ plot/pca.py
@@ -14,25 +14,27 @@ vecs = model.load(path.realpath(sys.argv[1]))
 model = sklearn.decomposition.PCA()
 PCs = model.fit_transform(vecs)
 
-fig = plt.figure()
+print('variance explained: ', np.sum(model.explained_variance_ratio_[0:4] * 100))
+
+fig = plt.figure(figsize=(8, 6))
 gs = gridspec.GridSpec(3, 2)
 
 # PC[0], PC[1]
-plt.subplot(gs[0:2, 0])
+ax = plt.subplot(gs[0:2, 0])
 plt.scatter(PCs[:, 0], PCs[:, 1], lw=0, alpha=0.5, c='SteelBlue')
-plt.xlabel('PC[0]')
-plt.ylabel('PC[1]')
+plt.xlabel('PC 0')
+plt.ylabel('PC 1')
 
 # PC[2], PC[3]
-plt.subplot(gs[0:2, 1])
+ax = plt.subplot(gs[0:2, 1])
 plt.scatter(PCs[:, 2], PCs[:, 3], lw=0, alpha=0.5, c='SteelBlue')
-plt.xlabel('PC[2]')
-plt.ylabel('PC[3]')
+plt.xlabel('PC 2')
+plt.ylabel('PC 3')
 
 plt.subplot(gs[2, :])
-plt.plot(np.arange(0, PCs.shape[1]), model.explained_variance_ratio_, color='IndianRed')
+plt.plot(np.arange(0, PCs.shape[1]), model.explained_variance_ratio_ * 100, color='IndianRed')
 plt.xlabel('PC')
-plt.ylabel('ratio')
+plt.ylabel('explained [%]')
 
 fig.set_tight_layout(True)
 plt.show()
